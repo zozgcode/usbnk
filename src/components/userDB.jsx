0ab2ebebc7f6e3077e65bf994c9d61c7ddf8b032
sunshine-web-coder <webdev4gaming@gmail.com>
2023-12-28 16:16:12 +0100

    new update

--- src/components/userDB.jsx
+++ src/components/userDB.jsx
@@ -3,7 +3,7 @@ const users = [
     id: 1,
     name: "Toby Keith",
     cardName: "Toby Keith",
-    username: "tobyk125",
+    username: "Tobyk125",
     password: "@44f!@9f132",
     amount: "40,000,000.00",
   },
